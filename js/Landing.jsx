124fbd0b808d2d8a33a1c7cf72a02545607096bb
Joshua Lee <jdl93@nau.edu>
2017-10-21 18:37:37 -1000

    all changes

---
+++ js/Landing.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Landing = () => (
+  <div className="landing">
+    <h1>Joshua's Video</h1>
+    <input type="text" placeholder="Search" />
+    <a>or Browse All</a>
+  </div>
+);
